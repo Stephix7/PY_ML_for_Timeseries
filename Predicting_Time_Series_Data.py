80a36d0f9beafe94324b919a18d73c5329190580
Stefan Jaspers <Stephix7@users.noreply.github.com>
2019-11-27 06:10:08 +0100

    Create Predicting_Time_Series_Data.py

---
+++ Predicting_Time_Series_Data.py
@@ -0,0 +1,6 @@
+# Introducing the dataset
+
+# Scatterplot with color relating to time
+prices.plot.scatter('EBAY', 'YHOO', c=prices.index, 
+                    cmap=plt.cm.viridis, colorbar=False)
+plt.show()
