b3f5600ecfc85d3b163f7a97bbbad16f94165766
Stefan Jaspers <Stephix7@users.noreply.github.com>
2019-11-27 05:24:32 +0100

    Update Primer.py

--- Primer.py
+++ Primer.py
@@ -5,3 +5,106 @@ fig, axs = plt.subplots(2, 1, figsize=(5, 10))
 data.iloc[:1000].plot(y='data_values', ax=axs[0])
 data2.iloc[:1000].plot(y='data_values', ax=axs[1])
 plt.show()
+
+# Plotting a time series (II)
+
+# Plot the time series in each dataset
+fig, axs = plt.subplots(2, 1, figsize=(5, 10))
+data.iloc[:1000].plot(x='time', y='data_values', ax=axs[0])
+data2.iloc[:1000].plot(x='time', y='data_values', ax=axs[1])
+plt.show()
+
+# ------------------------------------------------------------------------
+# Fitting a simple model: classification
+# In this exercise, you'll use the iris dataset (representing petal characteristics of a number of flowers) to practice using the scikit-# learn API to fit a classification model. You can see a sample plot of the data to the right.
+
+from sklearn.svm import LinearSVC
+
+# Construct data for the model
+X = data[["petal length (cm)", "petal width (cm)"]]
+y = data[['target']]
+
+# Fit the model
+model = LinearSVC()
+model.fit(X, y)
+
+# ------------------------------------------------------------------------
+# Predicting using a classification model 
+
+# Create input array
+X_predict = targets[['petal length (cm)', 'petal width (cm)']]
+
+# Predict with the model
+predictions = model.predict(X_predict)
+print(predictions)
+
+# Visualize predictions and actual values
+plt.scatter(X_predict['petal length (cm)'], X_predict['petal width (cm)'],
+            c=predictions, cmap=plt.cm.coolwarm)
+plt.title("Predicted class values")
+plt.show()
+
+# ------------------------------------------------------------------------
+# Fitting a simple model: regression
+
+from sklearn import linear_model
+
+# Prepare input and output DataFrames
+X = boston[['AGE']]
+y = boston[['RM']]
+
+# Fit the model
+model = linear_model.LinearRegression()
+model.fit(X,y)
+
+# ------------------------------------------------------------------------
+# Predicting using a regression model
+
+# Generate predictions with the model using those inputs
+predictions = model.predict(new_inputs.reshape(-1,1))
+
+# Visualize the inputs and predicted values
+plt.scatter(new_inputs, predictions, color='r', s=3)
+plt.xlabel('inputs')
+plt.ylabel('predictions')
+plt.show()
+
+# ------------------------------------------------------------------------
+# Inspecting the classification data
+
+import librosa as lr
+from glob import glob
+
+# List all the wav files in the folder
+audio_files = glob(data_dir + '/*.wav')
+
+# Read in the first audio file, create the time array
+audio, sfreq = lr.load(audio_files[0])
+time = np.arange(0, len(audio)) / sfreq
+
+# Plot audio over time
+fig, ax = plt.subplots()
+ax.plot(time, audio)
+ax.set(xlabel='Time (s)', ylabel='Sound Amplitude')
+plt.show()
+
+# ------------------------------------------------------------------------
+# Inspecting the regression data
+
+# Read in the data
+data = pd.read_csv('prices.csv', index_col=0)
+
+# Convert the index of the DataFrame to datetime
+data.index = pd.to_datetime(data.index)
+print(data.head())
+
+# Loop through each column, plot its values over time
+fig, ax = plt.subplots()
+for column in data.columns:
+    data[column].plot(ax=ax, label=column)
+ax.legend()
+plt.show()
+
+
+  
+  
